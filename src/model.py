9ddd53acee80f459dd4fbdb03d1fddacd71e4bbe
Chuck-Hou Yee <chuckyee@gmail.com>
2017-07-25 02:04:25 -0400

    Implemented U-Net of arbitrary depth + initial debugging.

--- src/model.py
+++ src/model.py
@@ -1,21 +1,25 @@
 from __future__ import division, print_function
 
 
-from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D
+from keras.layers import Input, Conv2D, MaxPooling2D, Conv2DTranspose, Concatenate
 from keras.models import Model
-from keras import optimizers
+from keras import optimizers, utils
+from keras import backend as K
 
 import glob
 import patient
 
 
 def downsampling_block(input_tensor, filters, padding='valid'):
-    x = Conv2D(filters=filters, kernel_size=(3,3), padding=padding, activation='relu')(input_tensor)
-    x = Conv2D(filters=filters, kernel_size=(3,3), padding=padding, activation='relu')(x)
+    x = Conv2D(filters, kernel_size=(3,3), padding=padding, activation='relu')(input_tensor)
+    x = Conv2D(filters, kernel_size=(3,3), padding=padding, activation='relu')(x)
+    _, height, width, _ = K.int_shape(x)
+    assert(height % 2 == 0)
+    assert(width % 2 == 0)
     return MaxPooling2D(pool_size=(2,2))(x), x
 
 def upsampling_block(input_tensor, skip_tensor, filters, padding='valid'):
-    x = UpSampling2D(size=(2,2))(input_tensor)
+    x = Conv2DTranspose(filters, kernel_size=(2,2), strides=(2,2))(input_tensor)
     # compute amount of cropping needed for skip_tensor
     if padding == 'valid':
         _, x_height, x_width, _ = x.shape
@@ -27,11 +31,12 @@ def upsampling_block(input_tensor, skip_tensor, filters, padding='valid'):
     else:
         y = skip_tensor
     x = Concatenate()([x, y])
-    x = Conv2D(filters=filters, kernel_size=(3,3), padding=padding, activation='relu')(x)    
-    return Conv2D(filters=filters, kernel_size=(3,3), padding=padding, activation='relu')(x)    
+    x = Conv2D(filters, kernel_size=(3,3), padding=padding, activation='relu')(x)    
+    return Conv2D(filters, kernel_size=(3,3), padding=padding, activation='relu')(x)    
 
-def unet(height, width, padding):
-    x = Input(shape=(height, width))
+def unet_v1(height, width, padding='valid'):
+    inputs = Input(shape=(height, width, 1))
+    inputs = x
 
     x, x1 = downsampling_block(x, 64,  padding)
     x, x2 = downsampling_block(x, 128, padding)
@@ -46,29 +51,65 @@ def unet(height, width, padding):
     x = upsampling_block(x, x2, 128, padding)
     x = upsampling_block(x, x1, 64,  padding)
 
-    logits = Conv2D(filters=2, kernel_size=(1,1))(x)
+    logits = Conv2D(filters=classes, kernel_size=(1,1))(x)
 
-    return Model(inputs=inputs, outputs=logis)
+    return Model(inputs=inputs, outputs=logits)
+
+def unet_v2(height, width, features, depth, classes, padding='valid'):
+    x = Input(shape=(height, width, 1))
+    inputs = x
+
+    skips = []
+    for i in range(depth):
+        x, x0 = downsampling_block(x, features,  padding)
+        skips.append(x0)
+        features *= 2
+
+    x = Conv2D(filters=features, kernel_size=(3,3), padding=padding, activation='relu')(x)
+    x = Conv2D(filters=features, kernel_size=(3,3), padding=padding, activation='relu')(x)
+
+    for i in reversed(range(depth)):
+        features //= 2
+        x = upsampling_block(x, skips[i], features, padding)
+
+    logits = Conv2D(filters=classes, kernel_size=(1,1))(x)
+
+    return Model(inputs=inputs, outputs=logits)
 
 def main():
-    patient_dirs = glob.glob("~/Developer/datasets/cardiac-mri/TrainingSet/patient*")
-    images = []
-    masks = []
-    for patient_dir in patient_dirs:
-        p = patient.PatientData(patient_dir)
-        images.append(p.images)
-        masks.append(p.endocardium_masks)
-
-    height, width = 216, 256
     learning_rate = 0.01
     momentum = 0.99
     decay = 0.0
     epochs = 100
     validation_split = 0.2
     padding = 'same'
+    features = 32
+    depth = 3
+    classes = 2
 
-    height, width = images[0].shape
-    model = unet(height, width, padding)
+    import numpy as np
+    patient_dirs = glob.glob("/home/paperspace/Developer/datasets/RVSC/TrainingSet/patient*")[:1]
+    images = []
+    masks = []
+    for patient_dir in patient_dirs:
+        p = patient.PatientData(patient_dir)
+        images += p.images
+        masks += p.endocardium_masks
+    images = np.asarray(images)[:,:,:,None]
+    masks = np.asarray(masks) // 255
+    print(images.shape, masks.shape)
+    print(set(masks.flatten()))
+    dims = masks.shape
+    masks = utils.to_categorical(masks).reshape(*dims, classes)
+    print(masks.shape)
+
+    _, height, width, _ = images.shape
+    print(height, width)
+    model = unet_v2(height, width, features, depth, classes, padding)
     optimizer = optimizers.SGD(lr=learning_rate, momentum=momentum, decay=decay)
-    model.compile(optimizer=optimizer, loss='mean_squared_error', metrics=['accuracy'])
+    model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
     model.fit(images, masks, epochs=epochs, validation_split=validation_split)
+
+
+if __name__ == '__main__':
+    main()
