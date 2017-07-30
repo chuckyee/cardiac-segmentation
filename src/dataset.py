757031bf0c2af7d65358752138cdb9e50c727bc6
Chuck-Hou Yee <chuckyee@gmail.com>
2017-07-29 22:25:16 -0400

    Implement simple image augmentation (rotate, shift, shear, zoom).

--- src/dataset.py
+++ src/dataset.py
@@ -6,22 +6,13 @@ import numpy as np
 from math import ceil
 
 from keras import utils
+from keras.preprocessing import image as keras_image
 from keras.preprocessing.image import ImageDataGenerator
 
 import patient
 
 
-def elastic_deformation(a, sigma):
-    augmentation_args = {
-        'rotation_range': 10,
-        'width_shift_range': 0.2,
-        'height_shift_range': 0.2,
-        'shear_range': 0.1,
-        'zoom_range': 0.1,
-        'fill_mode' : 'reflect',
-    }
-
-def create_generators(data_dir, batch_size, validation_split=0.0):
+def load_images(data_dir):
     glob_search = os.path.join(data_dir, "patient*")
     patient_dirs = glob.glob(glob_search)
 
@@ -41,6 +32,79 @@ def create_generators(data_dir, batch_size, validation_split=0.0):
     dims = masks.shape
     classes = len(set(masks[0].flatten())) # get num classes from first image
     masks = utils.to_categorical(masks).reshape(*dims, classes)
+
+    return images, masks
+
+def random_elastic_deformation(x, a, sigma):
+    return x
+
+class Iterator(object):
+    def __init__(self, images, masks, batch_size):
+        self.images = images
+        self.masks = masks
+        self.i = 0
+        self.batch_size = batch_size
+        augmentation_args = {
+            'rotation_range': 180,
+            'width_shift_range': 0.1,
+            'height_shift_range': 0.1,
+            'shear_range': 0.1,
+            'zoom_range': 0.05,
+            'fill_mode' : 'reflect',
+        }
+        self.idg = ImageDataGenerator(**augmentation_args)
+
+    def __next__(self):
+        return self.next()
+
+    def next(self):
+        start = self.i
+        end = min(start + self.batch_size, len(self.images))
+        self.i += self.batch_size
+        if self.i >= len(self.images):
+            self.i = 0
+        augmented_images = []
+        augmented_masks = []
+        images_masks = zip(self.images[start:end], self.masks[start:end])
+        for image, mask in images_masks:
+            _, _, channels = image.shape
+            stacked = np.concatenate((image, mask), axis=2)
+            augmented = self.idg.random_transform(stacked)
+            augmented = random_elastic_deformation(augmented, 0, 0)
+            augmented_images.append(augmented[:,:,:channels])
+            augmented_masks.append(augmented[:,:,channels:])
+        return np.asarray(augmented_images), np.asarray(augmented_masks)
+
+def create_generators(data_dir, batch_size, validation_split=0.0):
+    images, masks = load_images(data_dir)
+
+    # split out last %(validation_split) of images as validation set
+    split_index = int((1-validation_split) * len(images))
+
+    # Augment images and masks from training set
+    train_generator = Iterator(images[:split_index], masks[:split_index],
+                               batch_size)
+
+    train_steps_per_epoch = ceil(split_index / batch_size)
+
+    # Do not augment validation data
+    if validation_split > 0.0:
+        val_generator = ImageDataGenerator().flow(
+            images[split_index:],
+            masks[split_index:],
+            batch_size=batch_size)
+    else:
+        val_generator = None
+
+    val_steps_per_epoch = ceil((len(images) - split_index) / batch_size)
+
+    return (train_generator, train_steps_per_epoch,
+            val_generator, val_steps_per_epoch)
+
+
+def create_generators_keras(data_dir, batch_size, validation_split=0.0):
+    # Basic image / mask loader using keras; no image augmentation
+    images, masks = load_images(data_dir)
 
     # split out last %(validation_split) of images as validation set
     split_index = int((1-validation_split) * len(images))
--- src/test_dataset.py
+++ src/test_dataset.py
@@ -5,7 +5,19 @@ import unittest
 import dataset
 
 class TestDataset(unittest.TestCase):
-    def test_generator_creation(self):
+    def test_keras_generator(self):
+        self._test_generator(dataset.create_generators_keras)
+
+    def test_keras_no_validation(self):
+        self._test_no_validation(dataset.create_generators_keras)
+
+    def test_generator(self):
+        self._test_generator(dataset.create_generators)
+
+    def test_no_validation(self):
+        self._test_no_validation(dataset.create_generators)
+
+    def _test_generator(self, create_generators):
         data_dir = "../test-assets/"
         batch_size = 2
         validation_split = 0.5
@@ -13,7 +25,7 @@ class TestDataset(unittest.TestCase):
         # training image and 2 validation images
 
         (train_generator, train_steps_per_epoch,
-         val_generator, val_steps_per_epoch) = dataset.create_generators(
+         val_generator, val_steps_per_epoch) = create_generators(
              data_dir, batch_size, validation_split)
 
         self.assertEqual(train_steps_per_epoch, 1)
@@ -27,13 +39,13 @@ class TestDataset(unittest.TestCase):
         self.assertEqual(images.shape, (2, 216, 256, 1))
         self.assertEqual(masks.shape, (2, 216, 256, 2))
 
-    def test_no_validation(self):
+    def _test_no_validation(self, create_generators):
         data_dir = "../test-assets/"
         batch_size = 2
         validation_split = 0.0
 
         (train_generator, train_steps_per_epoch,
-         val_generator, val_steps_per_epoch) = dataset.create_generators(
+         val_generator, val_steps_per_epoch) = create_generators(
              data_dir, batch_size, validation_split)
 
         self.assertEqual(train_steps_per_epoch, 2)
