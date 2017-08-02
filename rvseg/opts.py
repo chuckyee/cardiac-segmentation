0b5d8ce4bd1fbf704e4cab1332f3c83369c71b5b
Chuck-Hou Yee <chuckyee@gmail.com>
2017-08-02 00:45:55 -0400

    Allow user to input INI config file, as well as commandline args.

--- rvseg/opts.py
+++ rvseg/opts.py
@@ -1,35 +1,113 @@
 from __future__ import division, print_function
 
+import os
 import argparse
+import configparser
+import logging
 
 definitions = [
-    (('-d', '--data_dir'),    (str,   '.',    "Directory containing patientXX/ directories")),
-    (('-o', '--outfile'),     (str,   'weights-final.hdf5', "Directory to write output data")),
-    (('--features',),         (int,   64,     "Number of features maps after first convolutional layer.")),
-    (('--depth',),            (int,   4,      "Number of downsampled convolutional blocks.")),
-    (('--temperature',),      (float, 1.0,    "Temperature of final softmax layer in model.")),
-    (('--padding',),          (str,   'same', "Padding in convolutional layers. Either `same' or `valid'.")),
-    (('--load_weights',),     (str,   None,   "Load model weights from specified file to initialize training.")),
-    (('--learning_rate',),    (float, None,   "Optimizer learning rate.")),
-    (('--momentum',),         (int,   None,   "Momentum for SGD optimizer.")),
-    (('--decay',),            (int,   None,   "Learning rate decay (not applicable for nadam).")),
-    (('--optimizer',),        (str,   'adam', "Optimizer: sgd, rmsprop, adagrad, adadelta, adam, adamax, or nadam.")),
-    (('--loss',),             (str,   'pixel', "Loss function: `pixel' for pixel-wise cross entropy, `dice' for dice coefficient.")),
-    (('-e', '--epochs'),      (int,   20,     "Number of epochs to train.")),
-    (('-b', '--batch_size'),  (int,   32,     "Mini-batch size for training.")),
-    (('--validation_split',), (float, 0.2,    "Percentage of training data to hold out for validation.")),
+    # model               type   default help
+    ('features',         (int,   64,     "Number of features maps after first convolutional layer.")),
+    ('depth',            (int,   4,      "Number of downsampled convolutional blocks.")),
+    ('temperature',      (float, 1.0,    "Temperature of final softmax layer in model.")),
+    ('padding',          (str,   'same', "Padding in convolutional layers. Either `same' or `valid'.")),
+    ('dropout',          (float, 0.0,    "Rate for dropout of activation units.")),
+    ('classes',          (str,   'inner', "One of `inner' (endocardium), `outer' (epicardium), or `both'.")),
+    ('batchnorm',        {'default': False, 'action': 'store_true', 
+                          'help': "Apply batch normalization before nonlinearities."}),
+
+    # loss
+    ('loss',             (str,   'pixel', "Loss function: `pixel' for pixel-wise cross entropy, `dice' for dice coefficient.")),
+    ('loss-weights',     {'type': float, 'nargs': '+', 'default': [0.1, 0.9],
+                          'help': "When using dice or jaccard loss, how much to weight each output class."}),
+
+    # training
+    ('epochs',           (int,   20,     "Number of epochs to train.")),
+    ('batch-size',       (int,   32,     "Mini-batch size for training.")),
+    ('validation-split', (float, 0.2,    "Percentage of training data to hold out for validation.")),
+    ('optimizer',        (str,   'adam', "Optimizer: sgd, rmsprop, adagrad, adadelta, adam, adamax, or nadam.")),
+    ('learning-rate',    (float, None,   "Optimizer learning rate.")),
+    ('momentum',         (float, None,   "Momentum for SGD optimizer.")),
+    ('decay',            (float, None,   "Learning rate decay (not applicable for nadam).")),
+
+    # files
+    ('datadir',          (str,   '.',    "Directory containing patientXX/ directories.")),
+    ('outdir',           (str,   '.',    "Directory to write output data.")),
+    ('outfile',          (str,   'weights-final.hdf5', "File to write final model weights.")),
+    ('load-weights',     (str,   '',     "Load model weights from specified file to initialize training.")),
+    ('checkpoint',       {'default': False, 'action': 'store_true',
+                          'help': "Write model weights after each epoch if validation accuracy improves."}),
+
+    # augmentation
+    ('augment-training', {'default': False, 'action': 'store_true',
+                          'help': "Whether to apply image augmentation to training set."}),
+    ('augment-validation', {'default': False, 'action': 'store_true',
+                            'help': "Whether to apply image augmentation to validation set."}),
+    ('rotation-range',     (float, 180,    "Rotation range (0-180 degrees)")),
+    ('width-shift-range',  (float, 0.1,    "Width shift range, as a float fraction of the width")),
+    ('height-shift-range', (float, 0.1,    "Height shift range, as a float fraction of the height")),
+    ('shear-range',        (float, 0.1,    "Shear intensity (in radians)")),
+    ('zoom-range',         (float, 0.05,   "Amount of zoom. If a scalar z, zoom in [1-z, 1+z]. Can also pass a pair of floats as the zoom range.")),
+    ('fill-mode',          (str,   'nearest', "Points outside boundaries are filled according to mode: constant, nearest, reflect, or wrap")),
+    ('alpha',              (float, 500,    "Random elastic distortion: magnitude of distortion")),
+    ('sigma',              (float, 20,     "Random elastic distortion: length scale")),
 ]
+
+def update_from_configfile(args, default, config, section, key):
+    # Point of this function is to update the args Namespace.
+    value = config.get(section, key)
+    if value == '' or value is None:
+        return
+
+    # Command-line arguments override config file values
+    if getattr(args, key) != default:
+        return
+
+    if isinstance(default, bool):
+        value = config.getboolean(section, key)
+    elif isinstance(default, int):
+        value = config.getint(section, key)
+    elif isinstance(default, float):
+        value = config.getfloat(section, key)
+    elif isinstance(default, str):
+        value = config.get(section, key)
+    elif isinstance(default, list):
+        # special case: loss-weights
+        string = config.get(section, key)
+        value = [float(x) for x in string.split()]
+    elif default is None:
+        # special case: optimizer parameters
+        value = config.getfloat(section, key)
+    setattr(args, key, value)
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Train U-Net to segment right ventricles from cardiac MRI images.")
-    for args, kwargs in definitions:
-        d = dict(zip(['type', 'default', 'help'], kwargs))
-        parser.add_argument(*args, **d)
 
-    parser.add_argument('--checkpoint', default=False, action='store_true',
-                        help="Write model weights after each epoch if validation accuracy improves.")
-    parser.add_argument('--batchnorm', default=False, action='store_true',
-                        help="Apply batch normalization before nonlinearities.")
+    for argname, kwargs in definitions:
+        d = kwargs
+        if isinstance(kwargs, tuple):
+            d = dict(zip(['type', 'default', 'help'], kwargs))
+        parser.add_argument('--' + argname, **d)
+
+    # allow user to input configuration file
+    parser.add_argument('configfile', nargs='?', type=str,
+                        help='Load options from config file (command line arguments take precedence).')
+
     args = parser.parse_args()
+
+    if args.configfile:
+        logging.info("Loading options from config file: {}".format(args.configfile))
+        config = configparser.ConfigParser(
+            inline_comment_prefixes=['#', ';'], allow_no_value=True)
+        config.read(args.configfile)
+        for section in config:
+            for key in config[section]:
+                if key not in args:
+                    raise Exception("Unknown option {} in config file.".format(key))
+                update_from_configfile(args, parser.get_default(key),
+                                       config, section, key)
+
+    for k,v in vars(args).items():
+        logging.info("{:20s} = {}".format(k, v))
 
     return args
