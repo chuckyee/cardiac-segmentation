0a48e712b1e8dc042918e03582d6aaff864dfed5
Chuck-Hou Yee <chuckyee@gmail.com>
2017-08-22 14:48:58 -0400

    Playing with PyTorch for efficient dilated densenet.

--- dilated-densenet/train.py
+++ dilated-densenet/train.py
@@ -1,7 +1,14 @@
 from __future__ import print_function, division
 
-from densenet import DenseNet
+import logging
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
 from rvseg import dataset, loss, opts
+from densenet import DenseNet
+
 
 def select_optimizer(optimizer_name, optimizer_args):
     optimizers = {
@@ -57,98 +64,19 @@ def train():
     print(model)
 
 
-    criterion = "pixelwise cross entropy"
+    criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
     for epoch in range(args.epochs):
         for step in range(train_steps_per_epoch):
             images, masks = next(train_generator)
+            images = Variable(torch.FloatTensor(images))
+            masks = Variable(torch.FloatTensor(masks))
             masks_pred = model(images)
             loss = criterion(masks_pred, masks)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
         # check validation error
-
-    if args.load_weights:
-        logging.info("Loading saved weights from file: {}".format(args.load_weights))
-        m.load_weights(args.load_weights)
-
-    # instantiate optimizer, and only keep args that have been set
-    # (not all optimizers have args like `momentum' or `decay')
-    optimizer_args = {
-        'lr':       args.learning_rate,
-        'momentum': args.momentum,
-        'decay':    args.decay
-    }
-    for k in list(optimizer_args):
-        if optimizer_args[k] is None:
-            del optimizer_args[k]
-    optimizer = select_optimizer(args.optimizer, optimizer_args)
-
-    # select loss function: pixel-wise crossentropy, soft dice or soft
-    # jaccard coefficient
-    if args.loss == 'pixel':
-        def lossfunc(y_true, y_pred):
-            return loss.weighted_categorical_crossentropy(
-                y_true, y_pred, args.loss_weights)
-    elif args.loss == 'dice':
-        def lossfunc(y_true, y_pred):
-            return loss.sorensen_dice_loss(y_true, y_pred, args.loss_weights)
-    elif args.loss == 'jaccard':
-        def lossfunc(y_true, y_pred):
-            return loss.jaccard_loss(y_true, y_pred, args.loss_weights)
-    else:
-        raise Exception("Unknown loss ({})".format(args.loss))
-
-    def dice(y_true, y_pred):
-        batch_dice_coefs = loss.sorensen_dice(y_true, y_pred, axis=[1, 2])
-        dice_coefs = K.mean(batch_dice_coefs, axis=0)
-        return dice_coefs[1]    # HACK for 2-class case
-
-    def jaccard(y_true, y_pred):
-        batch_jaccard_coefs = loss.jaccard(y_true, y_pred, axis=[1, 2])
-        jaccard_coefs = K.mean(batch_jaccard_coefs, axis=0)
-        return jaccard_coefs[1] # HACK for 2-class case
-
-    metrics = ['accuracy', dice, jaccard]
-
-    m.compile(optimizer=optimizer, loss=lossfunc, metrics=metrics)
-
-    # automatic saving of model during training
-    if args.checkpoint:
-        if args.loss == 'pixel':
-            filepath = os.path.join(
-                args.outdir, "weights-{epoch:02d}-{val_acc:.4f}.hdf5")
-            monitor = 'val_acc'
-            mode = 'max'
-        elif args.loss == 'dice':
-            filepath = os.path.join(
-                args.outdir, "weights-{epoch:02d}-{val_dice:.4f}.hdf5")
-            monitor='val_dice'
-            mode = 'max'
-        elif args.loss == 'jaccard':
-            filepath = os.path.join(
-                args.outdir, "weights-{epoch:02d}-{val_jaccard:.4f}.hdf5")
-            monitor='val_jaccard'
-            mode = 'max'
-        checkpoint = ModelCheckpoint(
-            filepath, monitor=monitor, verbose=1,
-            save_best_only=True, mode=mode)
-        callbacks = [checkpoint]
-    else:
-        callbacks = []
-
-    # train
-    logging.info("Begin training.")
-    m.fit_generator(train_generator,
-                    epochs=args.epochs,
-                    steps_per_epoch=train_steps_per_epoch,
-                    validation_data=val_generator,
-                    validation_steps=val_steps_per_epoch,
-                    callbacks=callbacks,
-                    verbose=2)
-
-    m.save(os.path.join(args.outdir, args.outfile))
 
 if __name__ == '__main__':
     train()
